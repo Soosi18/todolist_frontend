91d5d0bbf0ad26a81cfd261fddb3fbfb263715b6
Soosi18 <sarvagya.pandey2000@yahoo.com>
2024-11-21 16:32:01 +0530

    Add list context provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { UserContextProvider } from './context/userContext.jsx'
+import { ListContextProvider } from './context/listContext.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <UserContextProvider>
-      <App />
+      <ListContextProvider>
+        <App />
+      </ListContextProvider>
     </UserContextProvider>
   </StrictMode>,
 )
