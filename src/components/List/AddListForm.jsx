0db3080a180a562962f704bb5841b1d7f29dfa30
Soosi18 <sarvagya.pandey2000@yahoo.com>
2024-11-21 16:32:30 +0530

    Add form component for adding lists

--- src/components/List/AddListForm.jsx
+++ src/components/List/AddListForm.jsx
@@ -1,8 +1,95 @@
+import { useState } from "react";
+import { addList } from "../../utils/addList"
+import { Modal, Button, Form, Input, message } from "antd";
 
-const AddListForm = () => {
+export const AddListForm = ({ showListForm, setShowListForm, lists, setLists }) => {
+  const [listName, setListName] = useState('');
+  const [listStatus, setListStatus] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [messageApi, contextHolder] = message.useMessage();
+  const [form] = Form.useForm();
+
+  const success = (message) => {
+    messageApi.open({
+      type: "success",
+      message: message,
+    });
+  };
+
+  const error = (message) => {
+    messageApi.open({
+      type: "error",
+      message: message,
+    });
+  };
+
+  const onFinish = async () => {
+    setLoading(true);
+    const data = await addList(listName);
+    setLoading(false);
+
+    if (data.success) {
+      setListName('');
+      form.resetFields();
+      setLists([...lists, data.list]);
+      setShowListForm(!showListForm);
+      message.success(data.message);
+    }
+    else {
+      setListStatus('error');
+      message.error("Please Log In");
+    }
+  }
+
+  const onFinishFailed = () => {
+    setLoading(false);
+  };
+
+  const onCancel = () => {
+    setShowListForm(!showListForm);
+  };
+
+  const onListNameChange = (e) => {
+    setListName(e.target.value);
+    setListStatus('');
+  }
+
   return (
-    <div>AddListForm</div>
-  )
-}
+    <>
+      {contextHolder}
+      <Modal
+        open={showListForm}
+        onCancel={onCancel}
+        footer={null}
+        title="Add New List"
+      >
+        <Form
+          layout="vertical"
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          form={form}
+        >
+          <Form.Item
+            label="Name"
+            name="name"
+            rules={[
+              {
+                required: true,
+                message: "Please enter the list name",
+              },
+            ]}
+          >
+            <Input status={listStatus} onChange={(e) => onListNameChange(e)} />
+          </Form.Item>
+          <Form.Item>
+            <Button loading={loading} htmlType="submit">
+              Add
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
+    </>
+  );
+};
 
-export default AddListForm
+export default AddListForm;
