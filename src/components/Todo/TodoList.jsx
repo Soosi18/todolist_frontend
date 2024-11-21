ebb39342db43ef8266fc1e769aa292325705378e
Soosi18 <sarvagya.pandey2000@yahoo.com>
2024-11-21 16:36:08 +0530

    Add component that displays all tasks in selected list with edit/delete features

--- src/components/Todo/TodoList.jsx
+++ src/components/Todo/TodoList.jsx
@@ -1,8 +1,136 @@
+import { useState, useContext, useEffect } from "react";
+import { userContext } from "../../context/userContext";
+import { listContext } from "../../context/listContext";
+import { List, Button, Checkbox, Popconfirm, message } from "antd";
+import { getTodos } from "../../utils/getTodos";
+import { deleteTodo } from "../../utils/deleteTodo";
+import { EditTodoForm } from "./EditTodoForm"
+import { AddTodoForm } from "./AddTodoForm"
+import { editTodoStatus } from "../../utils/editTodoStatus";
 
 const TodoList = () => {
-  return (
-    <div>TodoList</div>
-  )
-}
+  const { selectedList } = useContext(listContext);
+  const { currentUser } = useContext(userContext);
+  const [todos, setTodos] = useState();
+  const [showEditForm, setShowEditForm] = useState(false);
+  const [showAddTodoForm, setShowAddTodoForm] = useState(false);
+  const [activeTodoId, setActiveTodoId] = useState();
+  const [activeTodoDescription, setActiveTodoDescription] = useState();
+  const [messageApi, contextHolder] = message.useMessage();
 
-export default TodoList
+  useEffect(() => {
+    const loadTodos = async () => {
+      if (selectedList) {
+        const data = await getTodos(selectedList);
+        if (data.success) {
+          setTodos(data.content);
+        }
+      }
+    };
+
+    loadTodos();
+
+    return () => {
+      setTodos();
+    };
+  }, [selectedList, currentUser]);
+
+  const success = (message) => {
+    message.open({
+      type: "success",
+      message: message,
+    });
+  };
+
+  const error = (message) => {
+    message.open({
+      type: "error",
+      message: message,
+    });
+  };
+
+  const handleEditTodo = (id, description) => {
+    setActiveTodoId(id);
+    setActiveTodoDescription(description);
+    setShowEditForm(!showEditForm);
+  };
+
+  const handleAddTodo = () => {
+    setShowAddTodoForm(!showAddTodoForm);
+  };
+
+  const handleDeleteTodo = async (item) => {
+    const data = await deleteTodo(selectedList, item.todo_id);
+    if (data.success) {
+      message.success(data.message);
+      setTodos(todos.filter((todo) => todo.todo_id !== item.todo_id));
+    } else {
+      message.error("Something went wrong. Please try again later");
+    }
+  };
+
+  const handleTodoStatusChange = async ({todo_id, description, is_complete}) => {
+    const data = await editTodoStatus(selectedList, todo_id, !is_complete);
+    if(data.success){
+      setTodos(todos.map((todo) => {
+        if(todo.todo_id === todo_id){
+          return {...todo, is_complete: !is_complete}
+        }
+        else{
+          return todo;
+        }
+      }))
+    }
+  }
+
+  return (
+    <>
+      {contextHolder}
+      <EditTodoForm 
+        showEditForm={showEditForm}
+        setShowEditForm={setShowEditForm}
+        todo_id={activeTodoId}
+        todo_description={activeTodoDescription}
+        todos={todos}
+        setTodos={setTodos}
+      />
+      <AddTodoForm 
+        showAddTodoForm={showAddTodoForm}
+        setShowAddTodoForm={setShowAddTodoForm}
+        todos={todos}
+        setTodos={setTodos}
+      />
+      {todos ? (<Button onClick={handleAddTodo}>Add Task</Button>) : null}
+      <List
+        bordered
+        dataSource={todos}
+        renderItem={(item) => {
+          return (
+            <List.Item
+              actions={[
+                <Button key="edit" onClick={() => handleEditTodo(item.todo_id, item.description)}>
+                  Edit
+                </Button>,
+                <Popconfirm
+                  key="delete"
+                  title="Delete Task"
+                  description="Are you sure you want to delete this task?"
+                  onConfirm={() => handleDeleteTodo(item)}
+                  okText="Yes"
+                  cancelText="No"
+                >
+                  <Button danger>Delete</Button>
+                </Popconfirm>,
+              ]}
+            >
+              <Checkbox key={item.todo_id} checked={item.is_complete} onChange={() => handleTodoStatusChange(item)}/>
+              {item.description}
+            </List.Item>
+          );
+        }}
+      />
+    </>
+  );
+};
+
+export default TodoList;
